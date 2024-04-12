ce02ca28e559b41ba27edc3a91ac5df59553e1b0
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-04-12 10:57:58 +0100

    added another emoji

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Under Construction 🛠️</h1>
+      <h1>Under Construction 🛠️👩‍🍳</h1>
     </main>
   );
 }

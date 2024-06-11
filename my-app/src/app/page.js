375297d715832e54dbcf398e01ff29f0ff9a203d
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-06-11 13:24:42 +0100

    edits to about

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,27 @@
 import "./page.css";
 
+import Image from "next/image";
+import AK from "/Images/AK.png";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="typewriter">Hello, I&apos;m Annamaria</h1>
+      <div className="mainContainer">
+        <div class="typewriter-container">
+          <h1 className="typewriter">Hello, I&apos;m Annamaria</h1>
+          <p>
+            A junior developer with full-stack skills and a passion for creating
+            technology for good.
+          </p>
+        </div>
+        <Image
+          className="me"
+          src={AK}
+          width={450}
+          height={450}
+          alt="Annamaria K"
+        />
+      </div>
     </main>
   );
 }

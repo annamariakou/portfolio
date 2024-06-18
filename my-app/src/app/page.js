9f79047b90031b5e908ec4716e7e06644201b8df
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-06-18 23:51:12 +0100

    added social links

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -3,6 +3,9 @@ import "./page.css";
 import Image from "next/image";
 import AK from "/Images/AK.png";
 import Projects from "/components/Projects/Projects.jsx";
+import Github from "/Images/Github.png";
+import Linkedin from "/Images/Linkedin.png";
+import Twitter from "/Images/Twitter.png";
 
 export default function Home() {
   return (
@@ -15,6 +18,17 @@ export default function Home() {
               // A junior developer with full-stack skills and a passion for
               creating technology for good.
             </p>
+          </div>
+          <div className="socials">
+            <a href="https://github.com/annamariakou" target="_blank">
+              <Image className="github" src={Github} alt="Github" />
+            </a>
+            <a href="https://www.linkedin.com/in/annamariakou/" target="_blank">
+              <Image className="linkedin" src={Linkedin} alt="Github" />
+            </a>
+            <a href="https://x.com/AnnamariaKou" target="_blank">
+              <Image className="twitter" src={Twitter} alt="Github" />
+            </a>
           </div>
         </div>
         <Image

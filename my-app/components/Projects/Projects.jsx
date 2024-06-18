642844a8d3cf9a37cbd6f9790e468703d35ada16
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-06-18 01:00:26 +0100

    added projects section

--- my-app/components/Projects/Projects.jsx
+++ my-app/components/Projects/Projects.jsx
@@ -2,7 +2,23 @@
 import "./Projects.css"
 
 export default function Projects() {
-<h1>
-    Project
-</h1>
+    return (
+        <div className="projectsContainer">
+<h2 className="projectsTitle">
+    PROJECTS
+</h2>
+
+<section className="cards">
+<article className="card">
+                   <p>content for card one</p>
+                </article>
+                <article className="card">
+                   <p>content for card one</p>
+                </article>
+                <article className="card">
+                   <p>content for card one</p>
+                </article>
+    </section>
+</div>
+)
 }

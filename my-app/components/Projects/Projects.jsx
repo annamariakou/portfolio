7c48a7a4d70d1d118d95c3c11aa08e6d60f1cd98
annamariakoutsoras <annamariakoutsoras@Annamarias-MacBook-Air>
2024-06-18 22:32:06 +0100

    added projects

--- my-app/components/Projects/Projects.jsx
+++ my-app/components/Projects/Projects.jsx
@@ -1,5 +1,9 @@
 "use client"
 import "./Projects.css"
+import Image from "next/image";
+import PlantSwapScreenshot from "/Images/PlantSwapScreenshot.png";
+import SoCBrainScreenshot from "/Images/SoCBrainScreenshot.png";
+import WeatherAppScreenshot from "/Images/WeatherAppScreenshot.png";
 
 export default function Projects() {
     return (
@@ -11,12 +15,27 @@ export default function Projects() {
 <section className="cards">
 <article className="card">
                    <p>SoCBrain</p>
+                   <Image
+                   className="images"
+                   src={SoCBrainScreenshot}
+                   alt="SocBrain"
+                   />
                 </article>
                 <article className="card">
                    <p>Weather App</p>
+                   <Image
+                   className="images"
+                   src={WeatherAppScreenshot}
+                   alt="WeatherApp"
+                   />
                 </article>
                 <article className="card">
-                   <p>Plant Sawp</p>
+                   <p>Plant Swap</p>
+                   <Image
+                   className="images"
+                   src={PlantSwapScreenshot}
+                   alt="Plant Swap"
+                   />
                 </article>
     </section>
 </div>
